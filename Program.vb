675c9159729975a393e874b8dc95719e9c833a23
Paul O'Neill <paul.oneill@abcellera.com>
2026-06-08 17:08:09 -0700

    Simplify for testing

--- Program.vb
+++ Program.vb
@@ -1,49 +1,26 @@
 Imports System
 Imports System.Runtime.InteropServices
+Imports batchsvr.Interop
 
 Module Program
     Sub Main(args As String())
-        Dim comObj As Object = Nothing
-        Dim selectedProgId As String = Nothing
-        Dim lastError As Exception = Nothing
+        Dim svr As BatchServer = Nothing
 
         Try
-            Dim candidateProgIds As String() = {
-                "BatchRemote.RemoteSupport",
-                "BatchRemote.RemoteSupport.1",
-                "BatchControl.BatchServer",
-                "BatchControl.BatchServer.1"
-            }
+            Dim strItem As String
+            Dim strReturn As String
 
-            For Each progId In candidateProgIds
-                Dim t = Type.GetTypeFromProgID(progId, throwOnError:=False)
-                If t Is Nothing Then
-                    Continue For
-                End If
+            svr = New BatchServerClass()
 
-                Try
-                    comObj = Activator.CreateInstance(t)
-                    selectedProgId = progId
-                    Exit For
-                Catch ex As Exception
-                    lastError = ex
-                End Try
-            Next
+            strItem = "TIME"
+            strReturn = svr.GetItem(strItem)
+            Console.WriteLine($"BatchServer.GetItem('{strItem}') returned: {strReturn}")
 
-            If comObj Is Nothing Then
-                Console.WriteLine("No supported FactoryTalk Batch ProgID found. Verify Batch Server components are installed and registered.")
-                If lastError IsNot Nothing Then
-                    Console.WriteLine($"Last activation error: {lastError.Message}")
-                End If
-                Return
-            End If
-
-            Console.WriteLine($"Created COM object via ProgID: {selectedProgId}")
         Catch ex As Exception
-            Console.WriteLine($"Failed to create BatchRemote: {ex.Message}")
+            Console.WriteLine($"Failed to create BatchServer: {ex.Message}")
         Finally
-            If comObj IsNot Nothing AndAlso Marshal.IsComObject(comObj) Then
-                Marshal.ReleaseComObject(comObj)
+            If svr IsNot Nothing AndAlso Marshal.IsComObject(svr) Then
+                Marshal.ReleaseComObject(svr)
             End If
         End Try
     End Sub
